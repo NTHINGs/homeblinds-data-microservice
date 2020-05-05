2ab3e14c49c52273320383e158bcd710219c378c
NTHINGs <n_othing@hotmail.com>
2020-05-05 16:29:07 -0500

    Adding db

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // Importar las dependencias para configurar el servidor
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
 
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+// Configuración MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/test?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // configurar el puerto y el mensaje en caso de exito
-app.listen((process.env.PORT || 5000), () => console.log('El servidor webhook esta escchando!'));
+app.listen((process.env.PORT || 5000), () => console.log('El servidor webhook esta escuchando!'));
 
 // Ruta de la pagina index
 app.get("/", function (req, res) {
-    res.send("Se ha desplegado de manera exitosa el CMaquera ChatBot :D!!!");
+    res.send("Se ha desplegado de manera exitosa el Microservice :D!!!");
 });
 
 // Facebook Webhook
@@ -35,9 +40,9 @@ app.post("/webhook", function (req, res) {
     // Verificar si el vento proviene del pagina asociada
     if (req.body.object == "page") {
         // Si existe multiples entradas entraas
-        req.body.entry.forEach(function(entry) {
+        req.body.entry.forEach(function (entry) {
             // Iterara todos lo eventos capturados
-            entry.messaging.forEach(function(event) {
+            entry.messaging.forEach(function (event) {
                 if (event.message) {
                     process_event(event);
                 }
@@ -49,44 +54,27 @@ app.post("/webhook", function (req, res) {
 
 
 // Funcion donde se procesara el evento
-function process_event(event){
+function process_event(event) {
     // Capturamos los datos del que genera el evento y el mensaje 
-    var senderID = event.sender.id;
-    var message = event.message;
-    
-    // Si en el evento existe un mensaje de tipo texto
-    if(message.text){
-        // Crear un payload para un simple mensaje de texto
-        var response = {
-            "text": 'Enviaste este mensaje: ' + message.text
-        }
-    }
-    
-    // Enviamos el mensaje mediante SendAPI
-    enviar_texto(senderID, response);
-}
+    const message = event.message;
 
-// Funcion donde el chat respondera usando SendAPI
-function enviar_texto(senderID, response){
-    // Construcicon del cuerpo del mensaje
-    let request_body = {
-        "recipient": {
-          "id": senderID
-        },
-        "message": response
-    }
-    
-    // Enviar el requisito HTTP a la plataforma de messenger
-    request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-          console.log('Mensaje enviado!')
-        } else {
-          console.error("No se puedo enviar el mensaje:" + err);
+    // Si en el evento existe un mensaje de tipo texto
+    if (message.text) {
+        // Crear un payload para almacenar el simple mensaje de texto
+        const payload = {
+            "text": message.text,
+            "timestamp": event.timestamp,
+            "sender": event.sender.id
         }
-    }); 
+        client.connect(err => {
+            const collection = client.db("homeblinds-data").collection("messenger-chats");
+
+            collection.insertMany([
+                payload
+            ], (err, result) => {
+                console.log("Mensaje insertado en la db!");
+                client.close();
+            });
+        });
+    }
 }
